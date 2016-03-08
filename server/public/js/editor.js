531e36806f3f0d1eb2ba8bb8382698e366affcfd
Chris Greenhalgh <chris.greenhalgh@nottingham.ac.uk>
2016-03-08 12:12:14 +0000

    editor show/hide detail; add name/description; add instrument setting

--- server/public/js/editor.js
+++ server/public/js/editor.js
@@ -1,4 +1,4 @@
-var editorApp = angular.module('editorApp', []);
+var editorApp = angular.module('editorApp', ['ngAnimate','ui.bootstrap']);
 
 editorApp.controller('EditorCtrl', ['$scope', '$http', function ($scope,$http) {
   $scope.filename = 'example.json';
@@ -30,12 +30,36 @@ editorApp.controller('EditorCtrl', ['$scope', '$http', function ($scope,$http) {
       }
     }
     $scope.markers = data.markers;
+    // vamp plugin instrument
+    if (data.parameters.vampParameters===undefined)
+      data.parameters.vampParameters = { instrument: 0 };
+    else if (data.parameters.vampParameters.instrument===undefined)
+      data.parameters.vampParameters.instrument = 0;
     $scope.parameters = data.parameters;
+    $scope.instrumentOptions = [
+	{name:"Multiple or unknown instruments",value:0},
+	{name:"Piano",value:1},
+	{name:"Guitar",value:2},
+	{name:"Violin",value:3},
+	{name:"Viola",value:4},
+	{name:"Cello",value:5},
+	{name:"Horn",value:6},
+	{name:"Flute",value:7},
+	{name:"Oboe",value:8},
+	{name:"Clarinet",value:9},
+	{name:"Tenor Sax",value:10},
+	{name:"Bassoon",value:11},
+	{name:"String quartet",value:12},
+	{name:"Wind ensemble",value:13}
+    ];
+    $scope.name = data.name;
+    $scope.description = data.description;
     console.log('read experience with '+(data.markers.length)+' markers');
   });
 }]);
 
 editorApp.controller('MarkerCtrl', ['$scope', function ($scope) {
+	$scope.isCollapsed = true;
 	console.log('new MarkerCtrl');
 	// nested...
 	$scope.addAction = function() {
@@ -47,5 +71,5 @@ editorApp.controller('MarkerCtrl', ['$scope', function ($scope) {
 	};
 	$scope.deleteAction = function(index) {
 		$scope.marker.actions.splice(index,1);  
-    };
+	};
 }]);
