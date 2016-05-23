b811f4ab955588bec3fde44af585096421beaa66
Chris Greenhalgh <chris.greenhalgh@nottingham.ac.uk>
2016-05-23 17:02:43 +0000

    more codeui test dev

--- server/public/js/codeui.js
+++ server/public/js/codeui.js
@@ -2,68 +2,121 @@ var codeui = angular.module('muzicodes.codeui', []);
 
 // Code node class
 codeui.factory('CodeNode', function() {
-	var nextId = 1;
-	function CodePart(type) {
+
+	function CodeToken(type) {
 		this.type = type;
-		this.id = nextId++;
 	}
-	CodePart.FIXED_TEXT = 1;
-	CodePart.NODE = 2;
-	CodePart.NOTE_NAME = 3;
-	CodePart.NOTE_ACCIDENTAL = 4;
-	CodePart.NUMBER = 5;
-	CodePart.prototype.toString = function() {
+	CodeToken.FIXED_TEXT = 1;
+	CodeToken.NODE = 2;
+	CodeToken.NOTE_NAME = 3;
+	CodeToken.NOTE_ACCIDENTAL = 4;
+	CodeToken.NUMBER = 5;
+	CodeToken.prototype.toString = function() {
 		switch(this.type) {
-		case CodePart.FIXED_TEXT:
-		case CodePart.NOTE_NAME:
-		case CodePart.NOTE_ACCIDENTAL:
+		case CodeToken.FIXED_TEXT:
+		case CodeToken.NOTE_NAME:
+		case CodeToken.NOTE_ACCIDENTAL:
 			return this.text;
-		case CodePart.NUMBER:
+		case CodeToken.NUMBER:
 			// TODO precision?
 			return this.number;
 		}
-		console.log('Warning: CodePart.toString for type '+this.type);
-	}
-
-	CodePart.newFixedText = function(text) {
-		var part = new CodePart(CodePart.FIXED_TEXT);
+		console.log('Warning: CodeToken.toString for type '+this.type);
+	};
+	CodeToken.prototype.createElement = function(ix, insertPoint) {
+		var el = angular.element('<div></div>').addClass('muz-part').data('ix', ix);
+		if (insertPoint && insertPoint[0]>0) {
+			// text before insert
+			var text = angular.element('<span></span>');
+			text.text(this.text.substring(0,insertPoint[0]));
+			el.append(text);
+		}
+		if (insertPoint) {
+			var text = angular.element('<span>&nbsp;</span>').addClass('muz-caret');
+			el.append(text);
+		}
+		if (insertPoint) {
+			// text after insert
+			var text = angular.element('<span></span>');
+			text.text(this.text.substring(insertPoint[0]));
+			el.append(text);
+		} else {
+			var text = angular.element('<span></span>');
+			text.text(this.text);
+			el.append(text);
+		}
+		this.el = el;
+		return el;
+	};
+	CodeToken.newFixedText = function(text) {
+		var part = new CodeToken(CodeToken.FIXED_TEXT);
 		part.text = text;
 		return part;
-	}
+	};
 	
 	function CodeNode(type) {
 		this.type = type;
+		// SEQUENCE, CHOICE, REPEAT:
 		this.children = [];
+		// NOTE:
+		// this.midinote = 60;
+		// this.frequency = ??
+		// DELAY
+		// this.beats = 1;
+		// this.seconds = ??
+		// NOTE_RANGE:
+		// this.minMidinote = 
+		// this.maxMidinote = 
+		// DELAY_RANGE
+		// this.minBeat = 
+		// this.maxBeats =
+		// REPEAT
+		// this.minRepeat =
+		// this.maxRepeat = 
 		this.initParts();
-		this.id = nextId++;
 	};
 	CodeNode.NOTE = 1;
 	CodeNode.DELAY = 2;
 	CodeNode.SEQUENCE = 3;
 	CodeNode.CHOICE = 4;
 	CodeNode.REPEAT = 5;
-	CodeNode.NOTE_RANGE = 6;
-	CodeNode.DELAY_RANGE = 7;
+	CodeNode.REPEAT_0_OR_MORE = 6;
+	CodeNode.REPEAT_1_OR_MORE = 7;
+	CodeNode.REPEAT_0_OR_1 = 8;
+	CodeNode.NOTE_RANGE = 9;
+	CodeNode.DELAY_RANGE = 10;
 	CodeNode.prototype.initParts = function() {
 		switch(this.type) {
 		case CodeNode.NOTE:
-			this.parts = [new CodePart(CodePart.NOTE_NAME), new CodePart(CodePart.NOTE_ACCIDENTAL)];
+			this.parts = [new CodeToken(CodeToken.NOTE_NAME), new CodeToken(CodeToken.NOTE_ACCIDENTAL)];
 			break;
 		case CodeNode.DELAY:
-			this.parts = [CodePart.newFixedText('/'), new CodePart(CodePart.NUMBER)];
+			this.parts = [CodeToken.newFixedText('/'), new CodeToken(CodeToken.NUMBER)];
 			break;
 		case CodeNode.SEQUENCE:
+			this.parts = [CodeToken.newFixedText('')];
+			// TODO children?
+			break;
 		case CodeNode.CHOICE:
-			this.parts = [CodePart.newFixedText('('), CodePart.newFixedText(')')];
+			this.parts = [CodeToken.newFixedText('('), CodeToken.newFixedText(')')];
 			// TODO children?
 			break;
 		case CodeNode.REPEAT:
-			this.parts = [new CodePart(CodePart.NOTE), CodePart.newFixedText('{'), CodePart.newFixedText(','), CodePart.newFixedText('}') ]
+			this.parts = [new CodeToken(CodeToken.NODE), CodeToken.newFixedText('{'), CodeToken.newFixedText(','), CodeToken.newFixedText('}') ]
 			// TODO min/max?
+			break;
+		case CodeNode.REPEAT_0_OR_1:
+			this.parts = [new CodeToken(CodeToken.NODE), CodeToken.newFixedText('?')];
+			break;
+		case CodeNode.REPEAT_0_OR_MORE:
+			this.parts = [new CodeToken(CodeToken.NODE), CodeToken.newFixedText('*')];
+			break;
+		case CodeNode.REPEAT_1_OR_MORE:
+			this.parts = [new CodeToken(CodeToken.NODE), CodeToken.newFixedText('+')];
 			break;
 		case CodeNode.NOTE_RANGE:
 		case CodeNode.DELAY_RANGE:
-			this.parts = [CodePart.newFixedText('['), CodePart.newFixedText('-'), CodePart.newFixedText(']')];
+			this.parts = [CodeToken.newFixedText('['), CodeToken.newFixedText('-'), CodeToken.newFixedText(']')];
 			// TODO min/max?
 			break;
 		default:
@@ -74,9 +127,25 @@ codeui.factory('CodeNode', function() {
 	CodeNode.prototype.getParts = function() {
 		return this.parts;
 	}
+	CodeNode.prototype.createElement = function(ix, insertPoint, selection) {
+		var el = angular.element('<div></div>').addClass('muz-node').data('ix', ix);
+		for (var pi in this.parts) {
+			var part = this.parts[pi];
+			var childInsert = null;
+			if (insertPoint && insertPoint[0]==pi)
+				childInsert = insertPoint.slice(1);
+			var child = part.createElement(pi, childInsert);
+			el.append(child);
+		}
+		this.el = el;
+		return el;
+	}
 	
 	CodeNode.newSequence = function() {
 		return new CodeNode(CodeNode.SEQUENCE);
+	}
+	CodeNode.newChoice = function() {
+		return new CodeNode(CodeNode.CHOICE);
 	}
 	return CodeNode;
 });
@@ -87,34 +156,35 @@ codeui.directive('muzCode', [function() {
 		scope: {
 			ngModel: '='
 		},
-		template: '<muz-term ng-model="ngModel"></muz-term>',
 		link: function(scope, element, attrs) {
-			scope.insertPoint = [];
-			scope.selection = [];
-		}
-	};
-}]);
-codeui.directive('muzTerm', [function() {
-	return {
-		restrict: 'E',
-		scope: {
-			ngModel: '='
-		},
-		template: '<muz-part ng-repeat="part in parts track by part.id" ng-model="part"></muz-part>',
-		link: function(scope, element, attrs) {
-			console.log('muzTerm with parts '+JSON.stringify(scope.ngModel.parts));
-			scope.parts = scope.ngModel.getParts();
-		}
-	};
-}]);
-codeui.directive('muzPart', [function() {
-	return {
-		restrict: 'E',
-		scope: {
-			ngModel: '='
-		},
-		template: '{{ngModel.toString()}}',
-		link: function(scope, element, attrs) {
+			element.attr('tabindex', 0);
+			// if the model is assigned we should rebuild the DOM representation
+			// Otherwise we will only deal with changes from the user, i.e. UI actions
+			function rebuild(code) {
+				// clear insert & selection
+				scope.insertPoint = [0,0];
+				scope.selection = null;
+				var el = code.createElement(0, scope.insertPoint, scope.selection);
+				element.empty();
+				element.append(el);
+			};
+			scope.$watch('ngModel', function(newValue) {
+				rebuild(newValue);
+			});
+			element.on('click', function(ev) {
+				//console.log('click...');
+				element[0].focus();
+			});
+			// focus / blur just css for now
+			element.on('keypress', function(ev) {
+				if(ev.charCode) {
+					console.log('keypress: '+ev.charCode);
+				}
+			});
+			// focus / blur just css for now
+			element.on('keydown', function(ev) {
+				console.log('keydown: '+ev.which);
+			});
 		}
 	};
 }]);
